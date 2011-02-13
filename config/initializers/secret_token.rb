8f57dd3c303618c3612ef80189a9a25269700262
Brent Beer <brent.beer@gmail.com>
2011-02-12 21:39:44 -0600

    forgot to add a shit ton more files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Timetracker::Application.config.secret_token = '07c3a6cd2412f676d2cc4a547d23cfb4fa0ca6b98b0ca46f9d496c16836b7ac66150223249d84033be39644b0fd9faf3c56281780219038448b35a866178fdd9'
